963495394762c8bc8764168d317b7266af158415
CCSS0923 <dmltjd4588@gmail.com>
2025-10-20 13:31:54 +0900

    added work2

---
+++ work2/JavaStudy1/src/Hello2.java
@@ -0,0 +1,17 @@
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕 자바야");
+		
+		int a, b, c, d;
+		a = 10;
+		b = 20;
+		c = a + b;
+		d = a - b;
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
